c3aaf852fd6b266c66cca1ae47e3ff3f6fb23a80
SSto <95592688+sstover25@users.noreply.github.com>
2022-05-16 00:01:37 -0500

    created and styled Resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,40 @@ import React from "react";
 
 function Resume() {
   return (
-    <div>
-      <div>Resume!!!</div>
+    <div className="section">
+      <h1 className="d-flex about-me-title px-5 pt-5 justify-content-center">
+        Resume
+      </h1>
+      <div className="d-flex flex-column p-3 align-items-center">
+        <p className="resume-text">
+          Download my{" "}
+          <a className="resume-link" href="#resume">
+            Resume
+          </a>
+        </p>
+        <div>
+          <span className="proficiencies">Front-End Proficiencies</span>
+          <ul className="proficiencies-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div>
+          <span className="proficiencies">Back-End Proficiencies</span>
+          <ul className="proficiencies-list">
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
